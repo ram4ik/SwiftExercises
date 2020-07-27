bfe6ea26bfe6cb98c62e1eecc3de3731951154ec
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-27 09:31:31 +0300

    sort unique symbols

---
+++ SwiftExercises/Exercises/UniqueSimbols.swift
@@ -0,0 +1,27 @@
+//
+//  UniqueSimbols.swift
+//  SwiftExercises
+//
+//  Created by ramil on 27.07.2020.
+//
+
+import Foundation
+
+func sortOne(input: String) -> Bool {
+    
+    var checkedSymbols = [Character]()
+    
+    for symbol in input {
+        if checkedSymbols.contains(symbol) {
+            return false
+        }
+        
+        checkedSymbols.append(symbol)
+    }
+    
+    return true
+}
+
+func sortTwo(input: String) -> Bool {
+    return Set(input).count == input.count
+}
