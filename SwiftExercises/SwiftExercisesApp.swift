2989e5bfdc82d7395511829ac3d47ba7bbc40d56
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-27 09:21:51 +0300

    Initial Commit

---
+++ SwiftExercises/SwiftExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExercisesApp.swift
+//  SwiftExercises
+//
+//  Created by ramil on 27.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
